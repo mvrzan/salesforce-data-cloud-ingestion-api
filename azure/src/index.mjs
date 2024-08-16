5b3318e4c23a4d6959b1661ef4aa63b88aeaa063
mvrzan <mvrzan@icloud.com>
2024-08-16 10:23:03 -0400

    add the token helper function

--- azure/src/index.mjs
+++ azure/src/index.mjs
@@ -1,184 +1,13 @@
-import { DefaultAzureCredential } from "@azure/identity";
-import { SecretClient } from "@azure/keyvault-secrets";
-import { CosmosClient } from "@azure/cosmos";
-import fetch from "node-fetch";
-import jwt from "jsonwebtoken";
-
-// create global variables to store the JWT token and its expiration time
-let cachedJwt; // JWT token
-let cachedJwtExpiresAt; // JWT token expiration time
-let dataCloudInstanceUrl; // Data Cloud instance URL
-let fetchedDataCloudInstanceUrl; // Data Cloud instance URL fetched from the token exchange
-let fetchedDataCloudToken; // Data Cloud token fetched from the token exchange
-
-// create global variables to store Azure Key Vault secrets
-let clientId;
-let username;
-let privateKey;
-let loginUrl;
-let ingestionSourceApiName;
-
-// Initialize Azure Key Vault client
-const credential = new DefaultAzureCredential();
-const keyVaultName = process.env.KEY_VAULT_NAME;
-const keyVaultUrl = `https://${keyVaultName}.vault.azure.net`;
-const secretClient = new SecretClient(keyVaultUrl, credential);
-
-// Initialize Cosmos DB client
-const cosmosEndpoint = process.env.COSMOS_DB_ENDPOINT;
-const cosmosKey = process.env.COSMOS_DB_KEY;
-const cosmosClient = new CosmosClient({
-  endpoint: cosmosEndpoint,
-  key: cosmosKey,
-});
-const databaseId = process.env.COSMOS_DB_DATABASE_ID;
-const containerId = process.env.COSMOS_DB_CONTAINER_ID;
+import { getSfToken } from "./getSfToken.mjs";
 
 export const handler = async (context, req) => {
   try {
     context.log("Azure Function handler called");
-
-    // Retrieve secrets from Azure Key Vault
-    const { value: clientIdSecret } = await secretClient.getSecret("CLIENT-ID");
-    const { value: usernameSecret } = await secretClient.getSecret("USERNAME");
-    const { value: privateKeySecret } = await secretClient.getSecret(
-      "RSA-PRIVATE-KEY"
-    );
-    const { value: loginUrlSecret } = await secretClient.getSecret("LOGIN-URL");
-    const { value: ingestionSourceApiNameSecret } =
-      await secretClient.getSecret("INGESTION-SOURCE-API-NAME");
-
-    // set global variables with the fetched secrets
-    clientId = clientIdSecret;
-    username = usernameSecret;
-    privateKey = privateKeySecret;
-    loginUrl = loginUrlSecret;
-    ingestionSourceApiName = ingestionSourceApiNameSecret;
-
-    // fetch the last JWT token from the Cosmos DB container
-    const { database } = await cosmosClient.databases.createIfNotExists({
-      id: databaseId,
-    });
-    const { container } = await database.containers.createIfNotExists({
-      id: containerId,
-    });
-    const { resources: items } = await container.items
-      .query({ query: "SELECT * FROM c" })
-      .fetchAll();
-
-    if (items.length >= 1) {
-      const lastRecord = items[items.length - 1];
-      cachedJwt = lastRecord.jwt;
-      cachedJwtExpiresAt = lastRecord.expires_at;
-      dataCloudInstanceUrl = lastRecord.dataCloudInstanceUrl;
-    }
-
-    // check if the token is still valid
-    if (
-      !cachedJwtExpiresAt ||
-      cachedJwtExpiresAt - Math.round(Date.now() / 1000) < 3600
-    ) {
-      context.log("Token is expired. Fetching a new token!");
-
-      // define jwt payload
-      const tokenPayload = {
-        iss: clientId,
-        sub: username,
-        aud: loginUrl,
-        exp: Math.round(Date.now() / 1000),
-      };
-
-      // decode base64 encoded rsa key from the Azure Key Vault
-      const rsaKey = Buffer.from(privateKey, "base64").toString("ascii");
-
-      // create and sign jwt
-      const token = jwt.sign(tokenPayload, rsaKey, {
-        algorithm: "RS256",
-      });
-
-      // Salesforce CRM Access Token Payload
-      const salesforceCrmTokenPayload = new URLSearchParams({
-        grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-        assertion: token,
-      });
-
-      // Salesforce CRM Access Token Request
-      const salesforceCrmResponse = await fetch(
-        `https://${loginUrl}/services/oauth2/token`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: salesforceCrmTokenPayload,
-        }
-      );
-
-      if (!salesforceCrmResponse.ok) {
-        throw new Error("HTTP error, status = " + salesforceCrmResponse.status);
-      }
-
-      const salesforceCrmResponseData = await salesforceCrmResponse.json();
-
-      // Data Cloud Token Exchange Payload
-      const dataCloudPayload = new URLSearchParams({
-        grant_type: "urn:salesforce:grant-type:external:cdp",
-        subject_token: salesforceCrmResponseData.access_token,
-        subject_token_type: "urn:ietf:params:oauth:token-type:access_token",
-      });
-
-      // Data Cloud Token Exchange Request
-      const dataCloudResponse = await fetch(
-        `${salesforceCrmResponseData.instance_url}/services/a360/token`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: dataCloudPayload,
-        }
-      );
-
-      if (!dataCloudResponse.ok) {
-        const status = dataCloudResponse.status;
-        const errorText = dataCloudResponse.statusText;
-        console.error("Data Cloud Token Exchange Error:", errorText);
-        throw new Error(
-          `HTTP error when exchanging token with Data Cloud, status = ${status}`
-        );
-      }
-
-      // parse the response from the Data Cloud token exchange
-      const dataCloudResponseData = await dataCloudResponse.json();
-      const dataCloudInstanceUrl = dataCloudResponseData.instance_url;
-
-      // store the fetched JWT token and its expiration time
-      fetchedDataCloudToken = dataCloudResponseData.access_token;
-      fetchedDataCloudInstanceUrl = dataCloudInstanceUrl;
-
-      // store the fetched JWT token and its expiration time in Cosmos DB
-      const tokenExpiration =
-        dataCloudResponseData.expires_in + Math.round(Date.now() / 1000);
-      const { database } = await cosmosClient.databases.createIfNotExists({
-        id: databaseId,
-      });
-      const { container } = await database.containers.createIfNotExists({
-        id: containerId,
-      });
-
-      await container.items.upsert({
-        jwt: fetchedDataCloudToken,
-        expires_at: tokenExpiration,
-        dataCloudInstanceUrl,
-      });
-    }
-
-    context.log("Token is valid. Proceeding with the request processing.");
+    const { token, dataCloudInstanceUrl, ingestionSourceApiName } =
+      await getSfToken();
 
     // Data Cloud Ingestion API URL
-    const dataCloudIngestionApiUrl = `https://${
-      dataCloudInstanceUrl ? dataCloudInstanceUrl : fetchedDataCloudInstanceUrl
-    }/api/v1/ingest/sources/${ingestionSourceApiName}/${req.query.objectName}`;
+    const dataCloudIngestionApiUrl = `https://${dataCloudInstanceUrl}/api/v1/ingest/sources/${ingestionSourceApiName}/${req.query.objectName}`;
     const body = req.body;
     const dataCloudObject = {
       data: [body],
@@ -190,9 +19,7 @@ export const handler = async (context, req) => {
       {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${
-            cachedJwt ? cachedJwt : fetchedDataCloudToken
-          }`,
+          Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
         body: JSON.stringify(dataCloudObject),
